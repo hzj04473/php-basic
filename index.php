8f9f419442578b18e73a6f8f18d9da79ef1501c5
Kim Jinkyoung <hzj04473@naver.com>
2025-12-10 20:50:37 +0900

    second commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>PHP를 공부합시다</h1>
+    <?php echo '안녕하세요'; ?>
+    <script>
+    document.write('안녕하세요');
+    </script>
+</body>
+
+</html>
