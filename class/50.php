e86b385d96588f1d34109d801a55a784290e0b86
Kim Jinkyoung <hzj04473@naver.com>
2025-12-13 14:43:21 +0900

    50. PHP OOP 클래스 만들기

---
+++ class/50.php
@@ -0,0 +1,40 @@
+<?php
+class Fruit
+{
+  // Properties
+  private $name;
+  public $color;
+
+  // Methods
+  function set_name($name)
+  {
+    $this->name = $name;
+  }
+
+  function get_name()
+  {
+    return $this->name;
+  }
+
+  function set_color($color)
+  {
+    $this->color = $color;
+  }
+
+  function get_color()
+  {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit();
+$banana = new Fruit();
+
+$apple->set_name('Apple');
+$banana->set_name('Banana');
+
+echo $apple->get_name();
+echo '<br />';
+echo $banana->get_name();
+echo '<br />';
+// echo $banana->name;
